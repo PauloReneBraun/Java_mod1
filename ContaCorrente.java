02b593822cdc1563542e6cd5d303ef6f00a817c0
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2024-08-29 16:20:09 +0100

    Add files via upload

---
+++ ContaCorrente.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class ContaCorrente {
+    int numero;
+    int agencia;
+    String nomeCliente;
+    LocalDate dataNascimento;
+    double saldo;
+    boolean ativo = true;
+
+    Double consultarSaldo(){
+        return  saldo;
+    }
+
+    List consultarExtrato (LocalDate dataInicial,
+                           LocalDate dataFinal){
+        return null;
+    }
+
+    void cancelar(String justificatica){
+        // algoritimo irá manipular o atributo
+        // ativo para que se torne false
+    }
+
+    void transferir (ContaCorrente contaDestino,
+                     double valorTransferido ){
+
+    }
+
+    void sacar (Double valorSolicitado){
+        // não solicitei
+    }
+}
